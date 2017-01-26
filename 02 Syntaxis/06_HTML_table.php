8562a8a4ed74822f09e165c1f6f0580b33cfa3c3
tpettrov <apetrov@vkp.prb>
2017-01-26 16:34:36 +0200

    solution

---
+++ 02 Syntaxis/06_HTML_table.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 1/26/2017
+ * Time: 12:53 PM
+ */
+
+
+$name = 'Gosho';
+$telNumber = '0882-321-423';
+$age = 24;
+$address = 'Suhata reka';
+
+echo
+"<table border='1'>
+<tr>
+    <td>Name</td>
+    <td>$name</td>
+</tr>
+    <tr>
+        <td>Number</td>
+        <td>$telNumber</td>
+    </tr>
+    <tr>
+        <td>Age</td>
+        <td>$age</td>
+    </tr>
+    <tr>
+        <td>Adress</td>
+        <td>$address</td>
+    </tr>
+</table>"
+?>
