7459c1345d34fde48f130db0c672f867a4202f7a
tpettrov <tpettrov@gmail.com>
2017-01-29 22:51:37 +0200

    problem 4 solved

---
+++ 03 Control flow/Exercise problems/Flow Control Loops/04_primes_html.php
@@ -0,0 +1,12 @@
+<form method="get">
+    <div>
+        Start index:
+        <input type="text" name="start">
+    </div>
+    <div>
+        End index:
+        <input type="text" name="end">
+    </div>
+    <input type="submit" name="print" value="Print Primes">
+
+</form>
--- 03 Control flow/Exercise problems/Flow Control Loops/04_show_primes_in_range.php
+++ 03 Control flow/Exercise problems/Flow Control Loops/04_show_primes_in_range.php
@@ -5,4 +5,55 @@
  * Date: 1/29/2017
  * Time: 21:38
  */
+include '04_primes_html.php';
+$array = [];
 
+if (isset($_GET['print']) && (!empty($_GET['start']) && !empty($_GET['end']) )) {
+
+    $start = intval($_GET['start']);
+    $end = intval($_GET['end']);
+
+    for ($i= $start; $i<=$end; $i++) {
+
+        if(isPrime($i)){
+
+            array_push($array,"<span><b>$i</b></span>" );
+        } else array_push($array,"<span>$i</span>");
+
+    }
+
+    echo implode(", ",$array);
+
+    }
+
+
+function isPrime($num) {
+    //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+    if($num == 1)
+        return false;
+
+    //2 is prime (the only even number that is prime)
+    if($num == 2)
+        return true;
+
+    /**
+     * if the number is divisible by two, then it's not prime and it's no longer
+     * needed to check other even numbers
+     */
+    if($num % 2 == 0) {
+        return false;
+    }
+
+    /**
+     * Checks the odd numbers. If any of them is a factor, then it returns false.
+     * The sqrt can be an aproximation, hence just for the sake of
+     * security, one rounds it to the next highest integer value.
+     */
+    $ceil = ceil(sqrt($num));
+    for($i = 3; $i <= $ceil; $i = $i + 2) {
+        if($num % $i == 0)
+            return false;
+    }
+
+    return true;
+}
