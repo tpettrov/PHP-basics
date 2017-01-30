415c1b20d9d96912e986728e48ebc1ee51fe76f0
tpettrov <apetrov@vkp.prb>
2017-01-30 17:00:54 +0200

    solutions

---
+++ 03 Control flow/Exercise problems/Flow Control Loops/06_modify_string.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 1/30/2017
+ * Time: 1:21 PM
+ */
+
+if (isset($_GET['submit']) && (!empty($_GET['input']))) {
+
+    $inputStr = $_GET['input'];
+    $command = $_GET['radio'];
+
+    switch ($command) {
+
+        case 'palindrome':
+            if (isPalindrome($inputStr)) {
+                $output = 'is Palindrome';
+            }else $output = 'is not Palindrome';
+            break;
+        case 'reverse':
+            break;
+        case 'split':
+            break;
+        case 'hash':
+            break;
+        case 'shuffle':
+            break;
+        default: break;
+
+    }
+
+
+}
+
+include '06_modify_string_html.php';
+
+function isPalindrome($str)
+{
+    $strToArr = str_split($str);
+    $strLen = count($strToArr);
+
+    for ($i=0; $i < $strLen/2; $i++){
+
+        if ($strToArr[$i] == $strToArr[($strLen-1) - $i]) {
+
+            return true;
+        }
+
+        else return false;
+
+    }
+
+}
+
---
+++ 03 Control flow/Exercise problems/Flow Control Loops/06_modify_string_html.php
@@ -0,0 +1,22 @@
+<form method="get">
+    <span>
+        Input string:
+        <input type="text" name="input">
+
+    </span>
+
+   <span><input type="radio" name="radio" value="palindrome">Check Palindrome</span>
+    <span><input type="radio" name="radio" value="reverse">Reverse String</span>
+    <span><input type="radio" name="radio" value="split">Split</span>
+    <span><input type="radio" name="radio" value="hash">Hash String</span>
+    <span><input type="radio" name="radio" value="shuffle">Shuffle String</span>
+
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
+<?php if (isset($output)) : ?>
+
+<div><?= $inputStr.$output?> </div>
+
+<?php endif;?>
