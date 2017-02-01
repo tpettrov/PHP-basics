db565e369a971e28f236bf78fad70ac39a55bfb5
tpettrov <apetrov@vkp.prb>
2017-02-01 09:08:05 +0200

    02 solved

--- 04 Arrays/Lecture problems/02_print_array_elements.php
+++ 04 Arrays/Lecture problems/02_print_array_elements.php
@@ -19,11 +19,21 @@
  * Time: 20:48
  */
 
-$array = [1,2,3,4,5,6];
+if(isset($_GET['submit']) && (!empty($_GET['input']))){
 
-    for ($i=0; $i< count($array) / 2; $i++) {
+    $array = explode(',', $_GET['input']);
+    $n = count($array) / 2;
+
+for ($i = 0; $i < $n ; $i++) {
+
+    if($i == count($array) - 1 - $i ) {
 
         echo $array[$i];
-        echo $array[count($array) -1 - $i];
+    } else
 
-    }
+    echo $array[$i]."\n".$array[count($array) - 1 - $i].'<br>';
+
+
+}
+
+}
