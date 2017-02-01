a78b52174bcc87c6eb448f631758607b0cbe8a57
tpettrov <apetrov@vkp.prb>
2017-02-01 11:10:11 +0200

    lecture problems solved

---
+++ 04 Arrays/Lecture problems/04_biggest_smallest_matrix.php
@@ -0,0 +1,77 @@
+<form method="get">
+    <div>
+        Input string:
+        <input type="text" name="input">
+
+    </div>
+
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 2/1/2017
+ * Time: 9:17 AM
+ */
+
+
+if (isset($_GET['submit']) && (!empty($_GET['input']))) {
+
+    $array = explode(' ', $_GET['input']);
+
+    $rows = count($array) / 3;
+    $cols = count($array) / $rows;
+
+    $arrayIndex = 0;
+    $arraySize = count($array);
+
+    $matrix = [];
+
+    for ($row = 0; $row < $rows; $row++) {
+
+        $matrix[$row] = [];
+
+        for ($col = 0; $col < $cols; $col++) {
+
+            $matrix[$row][$col] = $array[$arrayIndex];
+
+            if ($arrayIndex < $arraySize - 1) {
+
+                $arrayIndex++;
+            } else break;
+
+        }
+
+    }/*echo '<pre>';
+    print_r($matrix);
+    echo '</pre>'*/;
+
+    $biggest = $matrix[0][0];
+    $smallest = $matrix[0][0];
+
+    foreach ($matrix as $row) {
+
+        foreach ($row as $element) {
+
+            if ($element > $biggest) {
+
+                $biggest = $element;
+            }
+
+            if ($element < $smallest) {
+
+                $smallest = $element;
+            }
+
+        }
+
+    }
+
+    echo "Biggest number in the matrix is: $biggest ", '<br>';
+    echo "Smallest number in the matrix is: $smallest";
+
+
+}
