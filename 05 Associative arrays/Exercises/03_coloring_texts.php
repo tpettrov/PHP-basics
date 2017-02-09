1983602755519ce8921e9a3772294d031b537d37
tpettrov <tpettrov@gmail.com>
2017-02-10 00:04:32 +0200

    solution

--- 05 Associative arrays/Exercises/03_coloring_texts.php
+++ 05 Associative arrays/Exercises/03_coloring_texts.php
@@ -1,3 +1,8 @@
+<form>
+
+    <textarea name="input"></textarea>
+    <input type="submit"/>
+</form>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,4 +10,26 @@
  * Date: 2/9/2017
  * Time: 23:43
  */
+
+if (isset($_GET['input'])) {
+
+    $text = $_GET['input'];
+    $string = str_replace(' ', '', $text);
+
+    for ($i = 0; $i < strlen($string); $i++) {
+
+        if (ord($string[$i]) % 2 == 0) {
+
+            echo "<font color='red'>$string[$i] </font>";
+
+        }else {
+
+            echo "<font color='blue'>$string[$i] </font>";
+        }
+
+    }
+
+
+}
+
 
