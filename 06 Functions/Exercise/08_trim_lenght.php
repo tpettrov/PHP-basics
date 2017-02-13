5661ddfb451b93492dfdd48828554146b173c7aa
tpettrov <tpettrov@gmail.com>
2017-02-13 20:52:44 +0200

    sol

--- 06 Functions/Exercise/08_trim_lenght.php
+++ 06 Functions/Exercise/08_trim_lenght.php
@@ -9,12 +9,12 @@
 list($x1, $y1, $x2, $y2, $x3, $y3) = array_map('floatval', explode(', ', trim(fgets(STDIN))));
 
 
+function distanceCalc(float $x1, float $y1, float $x2, float $y2, float $x3, float $y3)
+{
 
-function distanceCalc (float $x1, float $y1, float $x2, float $y2, float $x3, float $y3){
-
-    $A = sqrt(($x1 - $x2)*($x1 - $x2) + ($y1 - $y2)*($y1 - $y2));
-    $B = sqrt(($x2 - $x3)*($x2 - $x3) + ($y2 - $y3)*($y2 - $y3));
-    $C = sqrt(($x1 - $x3)*($x1 - $x3) + ($y1 - $y3)*($y1 - $y3));
+    $A = sqrt(($x1 - $x2) * ($x1 - $x2) + ($y1 - $y2) * ($y1 - $y2));
+    $B = sqrt(($x2 - $x3) * ($x2 - $x3) + ($y2 - $y3) * ($y2 - $y3));
+    $C = sqrt(($x1 - $x3) * ($x1 - $x3) + ($y1 - $y3) * ($y1 - $y3));
 
     $max = max($A, $B, $C);
 
