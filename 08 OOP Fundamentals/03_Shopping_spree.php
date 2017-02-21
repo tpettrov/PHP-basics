82910ef3f0212c9f41da3c4fd943634547b4639e
tpettrov <apetrov@vkp.prb>
2017-02-21 17:02:44 +0200

    almost there

--- 08 OOP Fundamentals/03_Shopping_spree.php
+++ 08 OOP Fundamentals/03_Shopping_spree.php
@@ -30,8 +30,29 @@ class Person
     private function setMoney(float $money)
     {
 
-       Helper::validateMoney($money);
-       $this->money = $money;
+        Helper::validateMoney($money);
+        $this->money = $money;
+    }
+
+    public function buy(Product $product)
+    {
+
+        $this->bag[$product->getName()] = $product;
+        $this->money -= $product->getCost();
+
+    }
+
+    public function checkBalance(float $price)
+    {
+
+        // return true if has money, else return false
+
+        if ($this->money >= $price) {
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -47,23 +68,33 @@ class Product
     public function __construct(string $name, float $cost)
     {
         $this->setName($name);
-        $this->cost = $cost;
+        $this->setCost($cost);
 
     }
 
-    private function setName($name){
+    private function setName($name)
+    {
 
         Helper::validateName($name);
         $this->name = $name;
     }
 
-    private function setCost($cost){
+    private function setCost($cost)
+    {
 
         Helper::validateMoney($cost);
         $this->cost = $cost;
     }
 
+    public function getCost(){
 
+        return $this->cost;
+    }
+
+    public function getName(){
+
+        return $this->cost;
+    }
 
 
 }
@@ -97,4 +128,64 @@ class Helper
             throw new Exception('Money cannot be negative');
         }
     }
+
+
 }
+
+$people = [];
+$products = [];
+
+$peopleLine = explode(';', trim(fgets(STDIN)));
+$productLine = explode(';', trim(fgets(STDIN)));
+
+try {$people = inputBuilder($peopleLine, 'Person');
+    $products = inputBuilder($productLine, 'Product');
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+    $flag = true;
+}
+
+
+
+$input = trim(fgets(STDIN));
+
+while ($input != 'END' && !$flag) {
+
+
+    $commands = explode(' ', $input);
+
+    $personBuying = $commands[0];
+    $productWanted = $commands[1];
+
+    if ($people[$personBuying]->checkBalance($products[$productWanted]->getCost())){
+
+        $people[$personBuying]->buy($products[$productWanted]);
+        echo "$personBuying bought $productWanted" . PHP_EOL;
+    } else {
+
+        echo "$personBuying can't afford $productWanted" . PHP_EOL;
+    }
+
+
+    $input = trim(fgets(STDIN));
+}
+
+
+function inputBuilder($inputArray, string $type)
+{
+
+    $outputArray = [];
+
+    foreach (array_filter($inputArray) as $element) {
+
+        $data = explode('=', $element);
+        $outputArray[$data[0]] = new $type($data[0], $data[1]);
+
+    }
+
+    return $outputArray;
+
+}
+
+//print_r($people);
