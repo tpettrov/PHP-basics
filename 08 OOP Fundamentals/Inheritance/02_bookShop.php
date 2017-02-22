9b72924b3e7411e04fd4fae2c49ae710390d12a9
tpettrov <apetrov@vkp.prb>
2017-02-22 17:00:52 +0200

    final solution

--- 08 OOP Fundamentals/Inheritance/02_bookShop.php
+++ 08 OOP Fundamentals/Inheritance/02_bookShop.php
@@ -12,12 +12,13 @@ class Book {
     protected $author;
     protected $price;
 
+
     public function __construct(string $title, string $author, float $price)
 {
-    $this->setTitle($title);
-    $this->setAuthor($author);
-    $this->setPrice($price);
 
+        $this->setTitle($title);
+        $this->setAuthor($author);
+        $this->setPrice($price);
 
 }
 
@@ -65,9 +66,47 @@ protected function setTitle($title) {
 
     }
 
+    public function getPrice(){
+
+        return $this->price;
+    }
+
 
 
 }
 
-$book = new Book('tr', 'Zadsa Ivanova', 4);
 
+
+class GoldenEditionBook extends Book
+{
+
+    public function getPrice(){
+
+        return parent::getPrice() * 1.3;
+    }
+
+}
+
+$author = trim(fgets(STDIN));
+$title = trim(fgets(STDIN));
+$price = floatval(trim(fgets(STDIN)));
+$type = trim(fgets(STDIN));
+
+try {
+
+if ($type == 'STANDARD'){
+
+    $book = new Book($title, $author, $price);
+} else if ($type == 'GOLD') {
+
+    $book = new GoldenEditionBook($title, $author, $price);
+
+} else throw new Exception('Type is not valid!');
+
+echo 'OK' . PHP_EOL;
+echo $book->getPrice();
+
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+}
