cbffeb70e8ae746d5359ebb6426172690824fbba
tpettrov <apetrov@vkp.prb>
2017-02-23 15:45:10 +0200

    final

--- 08 OOP Fundamentals/Inheritance/04_online_radio_database.php
+++ 08 OOP Fundamentals/Inheritance/04_online_radio_database.php
@@ -1,12 +1,13 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: apetrov
  * Date: 2/23/2017
  * Time: 10:21 AM
  */
-
-class Song {
+class Song
+{
 
     private $artist;
     private $songName;
@@ -23,45 +24,178 @@ class Song {
 
     }
 
-    private function setArtist($artist){
+    private function setArtist($artist)
+    {
 
+        $stringSize = strlen($artist);
 
+        if ($stringSize <= 20 && $stringSize >= 3) {
+
+            $this->artist = $artist;
+        } else {
+            throw  new Exception('Artist name should be between 3 and 20 symbols.'. PHP_EOL);
+        }
 
 
     }
 
-    public function getLength(){
+    private function setName($songName)
+    {
 
-        return $this->songLength;
+        $stringSize = strlen($songName);
+
+        if ($stringSize <= 30 && $stringSize >= 3) {
+
+            $this->songName = $songName;
+        } else {
+            throw  new Exception('Song name should be between 3 and 30 symbols.'. PHP_EOL);
+        }
+
+
+    }
+
+    private function setLength($length)
+    {
+
+        $lengthArr = explode(':', $length);
+
+        if (count($lengthArr) == 2) {
+
+            $this->songLength = $lengthArr;
+            list($minutes, $seconds) = $lengthArr;
+
+            $secMin = $seconds + (60 * $minutes);
+
+            if ($secMin >=0 && $secMin <= 900 ) {
+
+                $this->setMinutes($minutes);
+                $this->setSeconds($seconds);
+
+            } else {
+
+                throw  new Exception('Invalid song length.' . PHP_EOL);
+            }
+
+        } else {
+
+            throw  new Exception('Invalid song length.' . PHP_EOL);
+        }
+
+
+    }
+
+    private function setMinutes($minutes)
+    {
+
+        if ($minutes >= 0 && $minutes <= 14) {
+
+            $this->songMinutes = $minutes;
+        } else {
+
+            throw new Exception('Song minutes should be between 0 and 14.' . PHP_EOL);
+        }
+
+    }
+
+    private function setSeconds($seconds)
+    {
+
+        if ($seconds >= 0 && $seconds <= 59) {
+
+            $this->songSeconds = $seconds;
+        } else {
+
+            throw new Exception('Song seconds should be between 0 and 59.' . PHP_EOL);
+        }
+    }
+
+    public function getMinutes()
+    {
+
+        return $this->songMinutes;
+    }
+
+    public function getSeconds()
+    {
+
+        return $this->songSeconds;
     }
 
 
 }
 
-class Playlist {
+class Playlist
+{
 
     private $songs = [];
-    private $totalLenght;
+    private $totalMinutes;
+    private $totalSeconds;
 
-    public function addSong(Song $song){
+    public function addSong(Song $song)
+    {
 
         $this->songs[] = $song;
+        $this->totalMinutes += $song->getMinutes();
+        $this->totalSeconds += $song->getSeconds();
+        echo 'Song added.' . PHP_EOL;
     }
 
-    public function getTotalLenght(){
 
-        foreach ($this->songs as $song) {
-
-            $this->totalLenght += $song->getLength();
-
-        }
-        
-        return $this->totalLenght;
-    }
-
-    public function getSongsCount(){
+    public function getSongsCount()
+    {
 
         return count($this->songs);
     }
 
+    public function __toString()
+    {
+        $seconds = $this->totalSeconds % 60;
+        $minutesToHours = $this->totalMinutes + ($this->totalSeconds / 60);
+        $minutes = floor($this->totalMinutes + ($this->totalSeconds / 60)) % 60;
+        $hours = floor($minutesToHours / 60);
+
+        $format = ('%01dh %02dm %02ds');
+        $print = sprintf($format, $hours, $minutes, $seconds);
+
+
+        return "Songs added: " . $this->getSongsCount() . PHP_EOL . "Playlist length: " . $print;
+    }
+
 }
+
+$songsCount = trim(fgets(STDIN));
+
+$playlist = new Playlist();
+
+
+
+    for ($i = 0; $i < $songsCount; $i++) {
+
+        try {
+        $songAttributes = explode(';', trim(fgets(STDIN)));
+
+        if (count($songAttributes) == 3) {
+
+            $artist = $songAttributes[0];
+            $songName = $songAttributes[1];
+            $length = $songAttributes[2];
+
+
+            $playlist->addSong(new Song($artist, $songName, $length));
+
+        } else {
+
+            throw new Exception('Invalid song.' . PHP_EOL);
+        }
+
+        } catch (Exception $e) {
+
+            echo $e->getMessage();
+        }
+
+
+    }
+
+
+
+echo $playlist;
