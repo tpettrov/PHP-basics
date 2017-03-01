8579dfcf28f7dbe2338989c09bf6d908a603f91a
tpettrov <apetrov@vkp.prb>
2017-03-01 16:47:22 +0200

    working

---
+++ 10 Forms and Sessions/04_html_tags_counter/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 3/1/2017
+ * Time: 4:40 PM
+ */
+
+
+if(isset($_GET['submit'])){
+
+    echo 'nq';
+
+}
+
+
+
+
+include('view.php');
---
+++ 10 Forms and Sessions/04_html_tags_counter/view.php
@@ -0,0 +1,14 @@
+
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: apetrov-->
+<!-- * Date: 3/1/2017-->
+<!-- * Time: 4:40 PM-->
+<!-- */-->
+
+<form>
+
+        Enter HTML tag: <input type="text" name="tag">
+    <input type="submit" name="submit"  >
+
+</form>
