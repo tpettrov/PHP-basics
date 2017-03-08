3e27527a3a65da819d3ec402b8be4c1353eedf64
tpettrov <tpettrov@gmail.com>
2017-03-08 18:47:05 +0200

    solution

--- 12 MySQL & PHP Basics/03_populate_table.php
+++ 12 MySQL & PHP Basics/03_populate_table.php
@@ -6,6 +6,13 @@
  * Time: 4:08 PM
  */
 
-$db = new PDO('mysql:host=localhost;dbname=minions', 'root', '', ['utf8_general_ci']);
+$db = new PDO('mysql:host=localhost;dbname=minions', 'root', '');
 
+
+$stmt = $db->prepare("INSERT INTO `minions` (`id`, `name`, `age`, `town_id`) VALUES ('2', 'Bob', '15', '3'),('1','Kevin','22','1'),('3','Steward','','2');");
+
+
+if (!$stmt->execute()) {
+    print_r($stmt->errorInfo());
+}
 
