d8bfaa73dc7d3c1e867e592771a2fb4a1d60ffc9
tpettrov <apetrov@vkp.prb>
2017-03-08 17:58:13 +0200

    begin

---
+++ 12 MySQL & PHP Basics/03_populate_table.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 3/8/2017
+ * Time: 4:08 PM
+ */
+
+$db = new PDO('mysql:host=localhost;dbname=minions', 'root', '', ['utf8_general_ci']);
+
+
