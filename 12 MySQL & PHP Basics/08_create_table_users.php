84281bd99dc7c98e5a8fb725f4e44d598750a677
tpettrov <tpettrov@gmail.com>
2017-03-09 00:43:43 +0200

    solved

--- 12 MySQL & PHP Basics/08_create_table_users.php
+++ 12 MySQL & PHP Basics/08_create_table_users.php
@@ -5,3 +5,21 @@
  * Date: 3/8/2017
  * Time: 23:12
  */
+
+$db = new PDO('mysql:host=localhost;dbname=minions', 'root', '', ['utf8_general_ci']);
+
+$stmt = $db->prepare("CREATE TABLE users (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  `username` TINYTEXT NOT NULL,
+  `password` CHAR NOT NULL,
+  `profile_picture` BLOB NULL,
+  `last_login_time` DATETIME,
+  `is_deleted` ENUM('true','false') NOT NULL
+  ) ");
+
+
+
+if (!$stmt->execute()) {
+    print_r($stmt->errorInfo());
+}
