8ecae6b4015bd5b9bc200587de71089e0aa5cdab
tpettrov <apetrov@vkp.prb>
2017-03-08 14:29:33 +0200

    solved

---
+++ 12 MySQL & PHP Basics/02_create_table.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apetrov
+ * Date: 3/8/2017
+ * Time: 11:07 AM
+ */
+
+$db = new PDO('mysql:host=localhost;dbname=minions', 'root', '', ['utf8_general_ci']);
+
+
+$stmt = $db->prepare("CREATE TABLE `minions` (
+	`id` INT NOT NULL,
+	`name` VARCHAR(50) NULL,
+	`age` TINYINT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'");
+$stmt->execute();
+
+$stmt = $db->prepare('CREATE TABLE `towns` (
+`id` INT NOT NULL,
+	`name` VARCHAR(50) NULL,
+	PRIMARY KEY(`id`)
+)');
+
+$stmt->execute();
+
+
