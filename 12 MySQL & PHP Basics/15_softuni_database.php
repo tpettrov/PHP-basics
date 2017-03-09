127e3b5dfa5d08dd062287ee65800d3b72229d95
tpettrov <tpettrov@gmail.com>
2017-03-09 23:26:06 +0200

    solved

--- 12 MySQL & PHP Basics/15_softuni_database.php
+++ 12 MySQL & PHP Basics/15_softuni_database.php
@@ -21,18 +21,23 @@ $db = new PDO('mysql:host=localhost;dbname=softuni', 'root', '');
 //VALUES
 // ('Sales'), ('Marketing'), ('Software Development'), ('Quality Assurance')");
 
-$stmt = $db->prepare("ALTER TABLE employees AUTO_INCREMENT=1;
-INSERT INTO `employees` 
-(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) 
-VALUES 
- ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', '4', '2013-01-02', '3500.00'),
- ('Petar ', 'Petrov', 'Petrov', 'Senior Engineer', '1', '2004-02-03', '4000.00'),
- ('Maria ', 'Petrova', 'Ivanova', 'Intern', '5', '2004-02-03', '525.25'),
- ('Georgi ', 'Terziev', 'Ivanov', 'CEO', '2', '2007-12-09', '3000.00'),
- ('Peter ', 'Pan', 'Pan', 'Intern', '3', '2007-12-09', '599.88')");
+//$stmt = $db->prepare("ALTER TABLE employees AUTO_INCREMENT=1;
+//INSERT INTO `employees`
+//(first_name, middle_name, last_name, job_title, department_id, hire_date, salary)
+//VALUES
+// ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', '4', '2013-01-02', '3500.00'),
+// ('Petar ', 'Petrov', 'Petrov', 'Senior Engineer', '1', '2004-02-03', '4000.00'),
+// ('Maria ', 'Petrova', 'Ivanova', 'Intern', '5', '2004-02-03', '525.25'),
+// ('Georgi ', 'Terziev', 'Ivanov', 'CEO', '2', '2007-12-09', '3000.00'),
+// ('Peter ', 'Pan', 'Pan', 'Intern', '3', '2007-12-09', '599.88')");
+
+$stmt = $db->prepare('SELECT * from `employees`');
 
 
 
 if (!$stmt->execute()) {
     print_r($stmt->errorInfo());
+} else {
+
+    var_dump($stmt->fetchAll($db::FETCH_ASSOC));
 }
