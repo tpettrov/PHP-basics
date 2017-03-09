edac9e6cbcb1f916aae5605343ea6d4f89e00497
tpettrov <apetrov@vkp.prb>
2017-03-09 16:30:21 +0200

    wokring save

--- 12 MySQL & PHP Basics/15_softuni_database.php
+++ 12 MySQL & PHP Basics/15_softuni_database.php
@@ -9,11 +9,28 @@
 
 $db = new PDO('mysql:host=localhost;dbname=softuni', 'root', '');
 
-$stmt = $db->prepare("ALTER TABLE towns AUTO_INCREMENT=1;
-INSERT INTO `towns` 
-(`name`) 
+//$stmt = $db->prepare("ALTER TABLE towns AUTO_INCREMENT=1;
+//INSERT INTO `towns`
+//(`name`)
+//VALUES
+//('Sofia'),('Plovdiv'),('Varna'),('Burgas')");
+
+//$stmt = $db->prepare("ALTER TABLE departments AUTO_INCREMENT=1;
+//INSERT INTO `departments`
+//(`name`)
+//VALUES
+// ('Sales'), ('Marketing'), ('Software Development'), ('Quality Assurance')");
+
+$stmt = $db->prepare("ALTER TABLE employees AUTO_INCREMENT=1;
+INSERT INTO `employees` 
+(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) 
 VALUES 
-('Sofia'),('Plovdiv'),('Varna'),('Burgas')");
+ ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', '4', '2013-01-02', '3500.00'),
+ ('Petar ', 'Petrov', 'Petrov', 'Senior Engineer', '1', '2004-02-03', '4000.00'),
+ ('Maria ', 'Petrova', 'Ivanova', 'Intern', '5', '2004-02-03', '525.25'),
+ ('Georgi ', 'Terziev', 'Ivanov', 'CEO', '2', '2007-12-09', '3000.00'),
+ ('Peter ', 'Pan', 'Pan', 'Intern', '3', '2007-12-09', '599.88')");
+
 
 
 if (!$stmt->execute()) {
